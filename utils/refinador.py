355880c9487834ee7706c9291a125c98365d1266
thepoisonman <vanklimen.contacto@gmail.com>
2025-04-18 14:21:39 -0300

    Add files via upload

--- utils/refinador.py
+++ utils/refinador.py
@@ -1,2 +1,31 @@
 
-# Por ahora refinamientos se manejan directo en app, futura ampliación posible
+import json
+import os
+
+def refinar_conceptos(df, memory_path):
+    if os.path.exists(memory_path):
+        with open(memory_path, 'r') as f:
+            memoria = json.load(f)
+    else:
+        memoria = {}
+
+    for i, row in df.iterrows():
+        cuit = str(row['CUIT'])
+        concepto_detectado = row['Concepto Detectado']
+        concepto_memorizado = memoria.get(cuit)
+        if concepto_memorizado:
+            df.at[i, 'Concepto Detectado'] = concepto_memorizado
+
+    return df
+
+def guardar_refinamientos(cambios, memory_path):
+    if os.path.exists(memory_path):
+        with open(memory_path, 'r') as f:
+            memoria = json.load(f)
+    else:
+        memoria = {}
+
+    memoria.update(cambios)
+
+    with open(memory_path, 'w') as f:
+        json.dump(memoria, f, indent=4)
